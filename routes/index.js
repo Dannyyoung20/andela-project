cb34b894d868deb96e199da68ecf8d5af84217b4
dansky <dan4allu93@gmail.com>
2017-10-15 22:05:33 -0700

    Handled the file upload

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
 var user = require("../middleware/user.middleware");
+
+const multerConf = {
+    storage: multer.diskStorage({
+        destination: function(req, file, next) {
+            next(null, 'public/uploads');
+        },
+        filename: function(req, file, next) {
+            const ext = file.mimetype.split('/')[1];
+            next(null, file.filename + '_'+ Date.now()+ '.'+ext);
+        }
+    }),
+    fileFilter: function(req, file, next) {
+        if(!file) {
+            next();
+        }
+        const image = file.mimetype.startWith('image/');
+        if(image){
+            next(null, true);
+        }else{
+            next({message: 'File not supported'}, false);
+        }
+    }
+}
+
 router.route('/')
     .get(function (req, res) {
         res.render('index');
     })
-    .post(user.postUser);
+    .post(multer(multerConf).single('photo'),user.postUser);
 
 router.route('/canvas')
-    .get(function(req, res) {
+    .get(function (req, res) {
         res.render('canvas');
     })
 
